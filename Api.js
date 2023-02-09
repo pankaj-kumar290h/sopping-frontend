dd77c215feebaaff1826fa75bdca2191b36df296
Pankaj Kumar <pankaj290h@gmail.com>
2023-02-09 12:48:07 +0530

    Update Api.js

--- Api.js
+++ Api.js
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
+
  export const BASE_URL = "https://sopping-backend.onrender.com/api";
-=======
- export const BASE_URL = "https://sopping-backend.onrender.com";
->>>>>>> c5a6666666f3d1387ae6ace6d32ec98575675839
+
